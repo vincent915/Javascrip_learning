68e1b9ef61a71f2dc2ec421417721d34eacfa521
vincent915 <vincentkao1988@gmail.com>
2021-01-26 23:44:32 +0800

    20210126_update

---
+++ Demo_1/app.js
@@ -0,0 +1,10 @@
+/*--------- Fisrt Javascript Function ---------*/
+function testFunction(){
+    document.getElementById("demo_3").innerHTML = "使用外部連結產生的結果";
+}
+
+/*--------- Second Javascript Function ---------*/
+var x = 5;
+var y = 5;
+var z = 6;
+document.getElementById("demo_5").innerHTML = (x == y) + "<br>" + (x == z);
